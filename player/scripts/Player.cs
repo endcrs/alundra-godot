f9df03154ef3df06b56561975522e8fc196eb66a
Endrew Cavalcante <end.cavalcant12@gmail.com>
2022-03-24 18:10:53 -0300

    player node

--- player/scripts/Player.cs
+++ player/scripts/Player.cs
@@ -2,6 +2,11 @@ using Godot;
 using System;
 public class Player : KinematicBody2D
 {
+    //Constantes
+    private const int ACCELERATION = 500;
+    private const int MAX_SPEED = 100;
+    private const int FRICTION = 500;
+    //Variáveis
     private Vector2 velocity = new Vector2(0, 0);
     private Vector2 input_vector = new Vector2(0, 0); 
 
@@ -15,15 +20,16 @@ public class Player : KinematicBody2D
         base._PhysicsProcess(delta);
         
         input_vector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
-        input_vector.x = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
+        input_vector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
+        input_vector = input_vector.Normalized();
 
         if (input_vector != new Vector2(0, 0)) {
-            velocity = input_vector;
+            velocity = velocity.MoveToward(input_vector * MAX_SPEED, ACCELERATION * delta);
         } else {
-            velocity = new Vector2(0, 0);
+            velocity = velocity.MoveToward(new Vector2(0, 0), FRICTION * delta);
         }
 
-        MoveAndCollide(velocity);
+        velocity = MoveAndSlide(velocity);
     }
 
 }
